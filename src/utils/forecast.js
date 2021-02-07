7d8191001fb20c1ccacb571b035e79d3b0d11a24
Keex Nenyiaba <YZ470P@EUR.corp.gm.com>
2021-02-07 10:05:15 +0000

    More forecast features

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-        const url = 'http://api.weatherstack.com/current?access_key=8247f6e5797065a03ab56c2056265ade&query=' + latitude + ',' + longitude + '&units=f'
+        const url = 'http://api.weatherstack.com/current?access_key=8247f6e5797065a03ab56c2056265ade&query=' + latitude + ',' + longitude 
 
             
         request({ url, json: true}, (error, {body}) => {
@@ -11,7 +11,8 @@ const forecast = (latitude, longitude, callback) => {
             }else if(body.error){
                 callback('Unable to find location')
             }else{
-                callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+                callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees celcius. It feels like ' + body.current.feelslike + 
+                ' degrees celcius.' + ' Precipitation ' + body.current.precip + 'cm.' + ' The wind direction is ' + body.current.wind_dir +' and has a speed of ' + body.current.wind_speed +'km/hr.')
             }
         })
     
